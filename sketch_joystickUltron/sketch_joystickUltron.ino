084086a20cf8ce5d87f3c8397d6cb710909cf952
Dannhz <danilocaetanobarreto@gmail.com>
2021-08-30 03:57:14 -0300

    Inclusão do acelerômetro MMA7361

--- sketch_joystickUltron/sketch_joystickUltron.ino
+++ sketch_joystickUltron/sketch_joystickUltron.ino
@@ -1,33 +1,53 @@
+//LIBS
 #include <SoftwareSerial.h>
+#include <AcceleroMMA7361.h>
 
 //VARIÁVEIS
   String comandoRec = ""; // comando recebido do celular.
   bool ligado = false; // liga ao desbloquear através do app mobile.
   unsigned long ultimoPing = 0; // armazena o momento em que foi realizado o ultimo ping.
+  unsigned long ultimoAngulo = 0; // armazena o momento em que houve a ultima verificação de angulo.
   unsigned long msAtual; // obtém o milissegundo atual em cada loop.
   int pingDelay; // tempo em ms 
+
+  int x;
+  int y;
+  int z;
   
   //CONSTANTES
   #define intervaloMs 2000 // tempo em ms para realizar o teste contínuo de latência.
+  #define intervaloAcelerometro 250 // tempo em ms para a verificação do ângulo do drone.
   #define btTx 10 // Pinagem do Tx
   #define btRx 11 // Pinagem do Rx
   
   //OBJETOS
   SoftwareSerial btSerial(btTx, btRx); 
+  AcceleroMMA7361 accelero;
 
 void setup() {
   Serial.begin(9600);
   btSerial.begin(9600);
+
+  Serial.begin(9600);
+  accelero.begin(4, 5, 6, 7, A0, A1, A2);
+  accelero.setARefVoltage(3.3);                   //3.3V para maior precisão
+  accelero.setSensitivity(LOW);                   //maior sensibilidade 
+  accelero.calibrate();
 }
 
 void loop() {
   msAtual = millis();
+  
   if(checkBt()) {comandoRec = cmdHandler(comandoRec);} // Caso haja um comando recebido, chama a função para tratá-lo.
   
   if(ligado){
     if(msAtual - ultimoPing >= intervaloMs){
       btSerial.println("{ping}");
       ultimoPing = msAtual;
+    }
+    
+    if(msAtual - ultimoAngulo >= intervaloAcelerometro){
+      printAngulos();
     }
   }
 }
@@ -72,4 +92,17 @@ String cmdHandler(String cmd){ //Tratamento do comando recebido pelo celular.
 void checkPing(){ // Recebe o retorno, calcula o delay e envia novamente para ser exibido na tela do celular.
   pingDelay = msAtual - ultimoPing;
   btSerial.println("[" + String(pingDelay) + "}");
+}
+
+void printAngulos(){ // Exibe o ângulo atual.
+  x = accelero.getXAccel();
+  y = accelero.getYAccel();
+  z = accelero.getZAccel();
+  Serial.print("x: ");
+  Serial.print(x);
+  Serial.print(" \ty: ");
+  Serial.print(y);
+  Serial.print(" \tz: ");
+  Serial.println(z);
+  ultimoAngulo = msAtual;
 }
